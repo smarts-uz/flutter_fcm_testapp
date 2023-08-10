cc83ad24bd347db74d79273fdde7c13b1aaf11fc
JasurbekErgashev <jergashev633@gmail.com>
2023-08-10 18:41:12 +0500

    added firebase app check

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:fcm_testapp/home_page.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -9,10 +10,29 @@ Future firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   debugPrint("Handling a background message: ${message.messageId}");
 }
 
+/// THIS IS AN EXAMPLE TO SEND APP CHECK TOKEN IN HEADER IN API REQUEST
+/// TOKEN MUST BE HANDLED ACCORDINGLY IN THE SERVER
+
+// void callApiExample() async {
+//   final appCheckToken = await FirebaseAppCheck.instance.getToken();
+//   if (appCheckToken != null) {
+//     final response = await http.get(
+//       Uri.parse("https://yourbackend.example.com/yourExampleEndpoint"),
+//       headers: {"X-Firebase-AppCheck": appCheckToken},
+//     );
+//   } else {
+//     // Error: couldn't get an App Check token.
+//   }
+// }
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.playIntegrity,
+    appleProvider: AppleProvider.deviceCheck,
   );
   runApp(const MyApp());
 }
